87bf78a9ce01f0220362796d14135a6ff2a0698a
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-11 20:31:28 +0900

    feat(Application): add 기본적인 의존성 주입, 프로그램 시작점

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,25 @@
 package store;
 
+import store.controller.StoreController;
+import store.model.repository.ProductRepository;
+import store.model.repository.PromotionRepository;
+import store.model.service.ReceiptCalculationService;
+import store.model.service.StoreService;
+import store.view.InputView;
+import store.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ProductRepository productRepository = new ProductRepository();
+        PromotionRepository promotionRepository = new PromotionRepository();
+
+        StoreService storeService = new StoreService(productRepository, promotionRepository);
+        ReceiptCalculationService receiptCalculationService = new ReceiptCalculationService();
+
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        StoreController storeController = new StoreController(inputView, outputView, storeService, receiptCalculationService);
+
+        storeController.run();
     }
 }
