7891f5a5c0f83552a9595a9d8d33afd1735665a8
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-11 20:30:44 +0900

    refactor(Discounts): add 최대 할인 금액 상수화

--- src/main/java/store/constant/Discounts.java
+++ src/main/java/store/constant/Discounts.java
@@ -5,4 +5,6 @@ public class Discounts {
     public static final String REGULAR_PRICE_MESSAGE_FORMAT = "\n현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)%n";
     public static final double MEMBERSHIP_DISCOUNT_RATE = 0.3;
     public static final String REGULAR = "null";
+    public static final int MAX_MEMBERSHIP_DISCOUNT = 8000;
+
 }
