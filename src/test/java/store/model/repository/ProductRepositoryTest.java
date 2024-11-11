96829cc398dec4538772a319024948f6a604aee9
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-11 20:06:13 +0900

    test(repository): 테스트 전용 파일 사용을 제거
    - 재고 추가 기능이 없으므로, 파일 수정 없이도 안정적으로 테스트 가능

--- src/test/java/store/model/repository/ProductRepositoryTest.java
+++ src/test/java/store/model/repository/ProductRepositoryTest.java
@@ -5,10 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import store.model.domain.Product;
 import store.constant.ProductType;
+import store.model.domain.Product;
 
-import java.io.IOException;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,14 +15,13 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class ProductRepositoryTest {
 
-    public static final String TEST_PRODUCT_FILE_PATH = "src/main/resources/testproducts.md";
     private ProductRepository productRepository;
 
     @Test
     @DisplayName("특정 타입과 이름으로 제품을 정확히 찾을 수 있다.")
-    void findInfo() throws IOException {
+    void findInfo() {
         // given
-        productRepository = new ProductRepository(TEST_PRODUCT_FILE_PATH);
+        productRepository = new ProductRepository();
 
         String name = "콜라";
 
@@ -62,9 +60,9 @@ class ProductRepositoryTest {
 
     @Test
     @DisplayName("getStock 메서드가 제품의 정확한 재고 수량을 반환한다.")
-    void findStock_ShouldReturnCorrectStockByTypeAndName() throws IOException {
+    void findStock_ShouldReturnCorrectStockByTypeAndName()  {
         // given
-        productRepository = new ProductRepository(TEST_PRODUCT_FILE_PATH);
+        productRepository = new ProductRepository();
 
         String name = "콜라";
 
@@ -77,9 +75,9 @@ class ProductRepositoryTest {
 
     @Test
     @DisplayName("reduceStock 메서드가 재고를 올바르게 감소시킨다.")
-    void reduceStock_ShouldDecreaseStockCorrectly() throws IOException {
+    void reduceStock_ShouldDecreaseStockCorrectly() {
         // given
-        productRepository = new ProductRepository(TEST_PRODUCT_FILE_PATH);
+        productRepository = new ProductRepository();
 
         String name = "콜라";
 
@@ -96,9 +94,9 @@ class ProductRepositoryTest {
 
     @Test
     @DisplayName("reduceStock 메서드가 재고 부족 시 감소를 거부한다.")
-    void reduceStock_ShouldFailWhenInsufficientStock() throws IOException {
+    void reduceStock_ShouldFailWhenInsufficientStock() {
         // given
-        productRepository = new ProductRepository(TEST_PRODUCT_FILE_PATH);
+        productRepository = new ProductRepository();
 
         String name = "콜라";
 
@@ -118,7 +116,7 @@ class ProductRepositoryTest {
     @MethodSource("provideProductReductionParameters")
     void reduceStock_ShouldHandleConsecutiveReductions(ProductType productType, String name, int firstReduce, int secondReduce, boolean expectedResult) {
         // given
-        productRepository = new ProductRepository(TEST_PRODUCT_FILE_PATH);
+        productRepository = new ProductRepository();
         int initialStock = productRepository.findStock(name, productType);
 
         // when
--- src/test/java/store/model/repository/PromotionRepositoryTest.java
+++ src/test/java/store/model/repository/PromotionRepositoryTest.java
@@ -1,46 +1,24 @@
 package store.model.repository;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import store.model.domain.Promotion;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.time.LocalDate;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class PromotionRepositoryTest {
 
-    public static final String TEST_PRODUCT_FILE_PATH = "src/main/resources/testpromotions.md";
     private PromotionRepository promotionRepository;
-
-    @Test
-    @DisplayName("마크다운 파일의 라인 수와 로드된 프로모션 개수가 일치하는지 검증한다.")
-    void loadPromotionsFromMarkdownFile() throws IOException {
-        // given
-        promotionRepository = new PromotionRepository(TEST_PRODUCT_FILE_PATH);
-
-        // 헤더 제외
-        int expectedLineCount = (int) Files.lines(Paths.get(TEST_PRODUCT_FILE_PATH)).skip(1).count();
-
-        // when
-        List<Promotion> actualPromotions = promotionRepository.findAll();
-
-        // then
-        assertThat(actualPromotions.size())
-                .isEqualTo(expectedLineCount); // 로드한 프로모션의 수가 파일의 라인 수와 같은지 확인
-    }
 
     @Test
     @DisplayName("이름으로 프로모션을 정확히 찾을 수 있다.")
     void findByName() throws IOException {
         // given
-        promotionRepository = new PromotionRepository(TEST_PRODUCT_FILE_PATH);
+        promotionRepository = new PromotionRepository();
 
         String name = "탄산2+1";
         Promotion expectedPromotion = new Promotion(name, 2, 1, LocalDate.parse("2024-01-01"), LocalDate.parse("2024-12-31"));
