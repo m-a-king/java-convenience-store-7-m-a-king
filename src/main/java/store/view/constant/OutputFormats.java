8ffcd574e0d0d7f214b3f74478440f791e264114
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-11 17:21:15 +0900

    refactor(OutputView): add 상수 관리 클래스
    - 출력에 사용하는 상수를 분류해서 알맞는 클래스로 관리

---
+++ src/main/java/store/view/constant/OutputFormats.java
@@ -0,0 +1,11 @@
+package store.view.constant;
+
+public class OutputFormats {
+    public static final String HEADER_FORMAT = "%-15s\t%-10s\t%-5s\n";
+    public static final String ITEM_FORMAT = "%-15s\t%-10d\t%,5d\n";
+    public static final String TOTAL_FORMAT = "%-25s\t\t%,5d\n";
+    public static final String DISCOUNT_FORMAT = "%-25s\t\t%5s\n";
+    public static final String PRODUCT_OUT_OF_STOCK_FORMAT = "- %s %,d원 %s %s%n";
+    public static final String PRODUCT_IN_STOCK_FORMAT = "- %s %,d원 %,d개 %s%n";
+    public static final String GIFT_ITEM_FORMAT = "%-15s\t%-5d\n";
+}
---
+++ src/main/java/store/view/constant/OutputMessages.java
@@ -0,0 +1,11 @@
+package store.view.constant;
+
+public class OutputMessages {
+    public static final String WELCOME_MESSAGE = "안녕하세요. W편의점입니다.";
+    public static final String CURRENT_PRODUCTS_MESSAGE = "현재 보유하고 있는 상품입니다.\n";
+    public static final String PURCHASE_PROMPT_MESSAGE = "\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
+    public static final String MEMBERSHIP_PROMPT_MESSAGE = "\n멤버십 할인을 받으시겠습니까? (Y/N)";
+    public static final String ADDITIONAL_PURCHASE_PROMPT_MESSAGE = "\n감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)";
+    public static final String NO_STOCK = "재고 없음";
+    public static final String EMPTY_STRING = "";
+}
